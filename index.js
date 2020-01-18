ef1f315e21385825c9f8556898196bcf18a6b50c
BearCode2020 <dvntbearden@gmail.com>
2020-01-17 20:36:04 -0500

    # commit for viewing, after second attempt

--- index.js
+++ index.js
@@ -77,8 +77,19 @@ getName("Leia");
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
  */
-function makeSmartPerson( /* code here */ ) {
-    /* code here */
+function makeSmartPerson(name) {
+    var obj = {
+        name: name,
+        sum: function addition(numb1, numb2) {
+            console.log(numb1 + numb2);
+            return numb1 + numb2;
+        },
+        talk: function speak() {
+            return "Hello my name is " + name ".";
+        },
+    }
+}
+return obj;
 }
 
 
